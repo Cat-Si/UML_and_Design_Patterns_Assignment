ea1ce1260d30d1dda12d8c91d4bc5aa87e11e0fd
Catherine <sheen.catherinem@gmail.com>
2023-04-23 17:51:57 +0100

    Removed to leave clean Navigation

--- src/main/java/domain/utility/UUIDGenerator.java
+++
@@ -1,9 +0,0 @@
-package domain.utility;
-
-import java.util.UUID;
-
-public class UUIDGenerator {
-    public static UUID generate() {
-        return UUID.randomUUID();
-    }
-}
--- src/main/java/globals/Ioc_Container.java
+++
@@ -1,4 +0,0 @@
-package globals;
-
-public class Ioc_Container {
-}
