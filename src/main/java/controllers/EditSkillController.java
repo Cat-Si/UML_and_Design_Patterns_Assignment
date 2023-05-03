7f0fb3bddfaa6ef7d19091c56d24bb2e91db847f
Catherine <sheen.catherinem@gmail.com>
2023-05-03 14:13:33 +0100

    Category breaks EditSkillController

--- src/main/java/controllers/EditSkillController.java
+++ src/main/java/controllers/EditSkillController.java
@@ -5,6 +5,7 @@ import domain.Category;
 import domain.Skill;
 import general.AlertMessage;
 import globals.Ioc_Container;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -34,13 +35,21 @@ public class EditSkillController implements DomainObjectToEdit {
     private final EditSkill editSkill = new EditSkill(Ioc_Container.getSkillRepository());
 
     private final GetAllCategories getAllCategories = new GetAllCategories(Ioc_Container.getCategoryRepository());
- /*   public void initialize() {
+    public void initialize() {
+        Platform.runLater(() -> skillName.requestFocus());
         showAllCategories();
-    }*/
+    }
+    @Override
+    public void passItemToEdit(Object skillToEdit) {
+        selectedSkill = (Skill) skillToEdit;
+        skillName.setText(selectedSkill.getSkillName());
+        categoryLst.getSelectionModel().select((selectedSkill.getCategory()));
+    }
+
     @FXML
     private void handleSubmitSkill(ActionEvent event) throws IOException {
         String name = skillName.getText();
-        Category category = categoryLst.getSelectionModel().getSelectedItem();
+       Category category = categoryLst.getSelectionModel().getSelectedItem();
         try {
             editSkill.requestList.add(category);
             editSkill.requestList.add(selectedSkill.getId());
@@ -52,18 +61,10 @@ public class EditSkillController implements DomainObjectToEdit {
         Router.changeRoute(RouteNames.SHOW_SKILL, event);
         }
 
-  /* private void showAllCategories() {
+    private void showAllCategories() {
         ObservableList<Category> items = FXCollections.observableArrayList(getAllCategories.execute());
         categoryLst.setItems(items);
-        categoryLst.getSelectionModel().select((selectedSkill.getCategory()));
-    }*/
-
-
-    @Override
-    public void passItemToEdit(Object skillToEdit) {
-        selectedSkill = (Skill) skillToEdit;
-        skillName.setText(selectedSkill.getSkillName());
-        categoryLst.getSelectionModel().select((selectedSkill.getCategory()));
     }
+
 }
 
