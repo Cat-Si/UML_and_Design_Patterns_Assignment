bc727236520969a139a69e575ef1248da1612591
Catherine <sheen.catherinem@gmail.com>
2023-06-12 20:37:16 +0100

    usecase

--- src/main/java/useCases/category/categoryFactory/UseCaseCommand.java
+++
@@ -1,9 +0,0 @@
-package useCases.category.categoryFactory;
-
-import Exceptions.EntryAlreadyExistsException;
-
-public interface UseCaseCommand {
-    void add(Object parameter);
-    void execute() throws IllegalArgumentException, EntryAlreadyExistsException;
-
-}
